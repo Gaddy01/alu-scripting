56a17fcbb0a43e1f2e728d9863031410c06c135d
Gaddy01 <g.irakoze2@alustudent.com>
2025-02-21 00:07:55 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,21 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditScript/0.1'}
-    params = {'limit': 10}  # Get only the first 10 hot posts
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-    
-    data = response.json()
-    
-    try:
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    except KeyError:
-        print(None)
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
