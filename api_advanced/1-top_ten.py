d103d6ec938395f77ac74c0671aac85463b75b64
Gaddy01 <g.irakoze2@alustudent.com>
2025-02-21 00:25:49 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,25 +3,13 @@
 import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-    Parameters:
-    subreddit (str): The name of the subreddit to query.
-    Returns:
-    None: If the subreddit is invalid or if an error occurs."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditBot/0.1'}
-    params = {'limit': 10}  # Ensure we only get 10 posts
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
-        return 
-    try:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        if not posts:
-            print(None)
-            return
-        for post in posts:
-            print(post['data']['title'])
-    except (KeyError, ValueError):
-        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
