09f5f2f2da82f31ed0d17cbf4e98851a69b8ee51
Gaddy01 <g.irakoze2@alustudent.com>
2025-02-21 00:38:11 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,8 +7,8 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}  # Limit to 10 posts
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    #params = {'limit': 10}  # Limit to 10 posts
+    response = requests.get(url, headers=headers, allow_redirects=False)
     # Check if the response is valid
     if response.status_code != 200:
         print("OK")  # Adjusted for test case expectation
