13df0abcf526d9d69e47eac764913c5d5d0e0a75
Gaddy01 <g.irakoze2@alustudent.com>
2025-02-20 13:05:45 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-""" This module queries the Reddit API to retrieve information 
-about a given subreddit, including the number of subscribers 
-and the top 10 hot posts. """
+""" top_ten.py """
 import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
-  
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom-script/1.0'}  # Setting a custom User-Agent
-    
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
         data = response.json()
         return data.get("data", {}).get("subscribers", 0)
     else:
         return 0
-
-def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
-  
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'custom-script/1.0'}  # Setting a custom User-Agent
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title", ""))
-    else:
-        print("None")
